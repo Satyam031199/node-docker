cb2eca427dddcd485819e8b16cd702f108a3d163
Satyam Chaturvedi <satyamchaturvedi71@gmail.com>
2024-12-27 13:03:13 +0530

    add mongoURI

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 const port = process.env.PORT || 3000;
+const mongoURI = "mongodb://admin:admin@172.25.0.2:27017/?authSource=admin";
 
-app.get('/', (req, res) => {
-    res.status(200).json({ message: 'Hello!' });
+mongoose
+  .connect(mongoURI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Hello!" });
 });
 
 app.listen(port, () => {
-    console.log(`App listening at http://localhost:${port}`);
+  console.log(`App listening at http://localhost:${port}`);
 });
+
